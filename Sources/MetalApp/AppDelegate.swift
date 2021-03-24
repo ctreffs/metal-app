4949d52799c779fbc48392e8427ec00b2785f060
Christian Treffs <ctreffs@gmail.com>
2021-03-24 08:29:43 +0100

    Unify main entry points

--- Sources/MetalApp/AppDelegate.swift
+++ Sources/MetalApp/AppDelegate.swift
@@ -12,17 +12,23 @@ import AppKit
 #endif
 import Foundation
 
-#if os(macOS)
 public final class AppDelegate: NSObject, APPLAppDelegate {
+    #if os(macOS)
     var window: APPLWindow!
+    #else
+    public var window: APPLWindow?
+    #endif
+
     lazy var viewController = MetalViewController()
 
+    #if os(macOS)
+    // MARK: - AppKit
     public func applicationDidFinishLaunching(_ notification: Notification) {
         viewController.view.frame.size.width = 800
         viewController.view.frame.size.height = 600
         let window = APPLWindow(contentViewController: viewController)
-        window.title = "MetalApp"
         self.window = window
+        window.title = "MetalApp"
         window.makeFirstResponder(nil)
         window.makeKeyAndOrderFront(nil)
         window.orderFrontRegardless()
@@ -32,18 +38,13 @@ public final class AppDelegate: NSObject, APPLAppDelegate {
     public func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         true
     }
-}
-#else
-@UIApplicationMain
-public final class AppDelegate: NSObject, APPLAppDelegate {
-    public var window: APPLWindow?
-    lazy var viewController = MetalViewController()
-
+    #else
+    // MARK: - UIKit
     public func applicationDidFinishLaunching(_ application: UIApplication) {
         let window = APPLWindow()
         window.rootViewController = viewController
         self.window = window
         window.makeKeyAndVisible()
     }
+    #endif
 }
-#endif
---
+++ Sources/MetalApp_iOS/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  MetalApp_iOS
+//
+//  Created by Christian Treffs on 24.03.21.
+//
+
+import UIKit
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
